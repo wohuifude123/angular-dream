76565079064622f45ee6e751f9745004cfc00b0a
univer <univer@AppledeMacBook-Pro.local>
2018-09-10 08:47:43 +0800

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { AppComponent } from "./app.component";
+export const routes: Routes = [ // Routes类型的数组
+    {
+        path      : 'index',
+        component : AppComponent
+    },{
+        path      : '',
+        redirectTo: 'index',
+        pathMatch : 'full'
+    }
+];
